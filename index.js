40ac593a3bfc027f27de1e5a436652e9190c0111
Yudi Alviska <yudi.a@weefer.co.id>
2022-11-01 14:51:01 +0700

    add sample response

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const wsServer = new WebSocket.Server({port: PORT});
 wsServer.on('connection', (socket) => {
   console.log('A client just connected');
   socket.on('message', (msg) => {
-    console.log('Receive message from client: ', msg);
+    console.log('Receive message from client: ', JSON.parse(msg));
     wsServer.clients.forEach((client) => {
-      client.send(msg);
+      const body = {Name: `Machine${Math.random(100)}`};
+      client.send(JSON.stringify(body));
     });
   });
 });
